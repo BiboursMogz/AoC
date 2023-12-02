5a81e2480eaa08ee07d512483a0ed095ce0613a6
BiboursMogz <antmaugas+github@gmail.com>
2023-12-02 17:52:55 +0100

    Initial commit
    Signed-off-by: BiboursMogz <antmaugas+github@gmail.com>

---
+++ 2023/day01/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	const char *testInput = 
+		"1abc2\
+		pqr3stu8vwx\
+		a1b2c3d4e5f\
+		treb7uchet";
+
+
+	printf("Hello, World!\n");
+	return 0;
+}
